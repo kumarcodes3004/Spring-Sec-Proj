7c176f6f2121327eaa4585be3dc6746dbb7a2792
ks9430565 <90494688+ks9430565@users.noreply.github.com>
2025-10-04 18:48:05 +0530

    Setting up project

---
+++ src/main/java/com/satyam/Spring_Sec_Proj/SpringSecProjApplication.java
@@ -0,0 +1,13 @@
+package com.satyam.Spring_Sec_Proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satyam/Spring_Sec_Proj/SpringSecProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyam.Spring_Sec_Proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
